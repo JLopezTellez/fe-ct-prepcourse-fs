173cf7f07ff8b556452c9875cdb6403804ae1d2c
JLopezTellez <jl764464@gmail.com>
2025-05-24 19:02:31 -0300

    Ejercicios C5 resueltos

--- C5 Fundamentos Javascript/ejercicios/09.js
+++ C5 Fundamentos Javascript/ejercicios/09.js
@@ -8,6 +8,12 @@ function esNuloOIndefinido(valor) {
   // 22 ---> false
   // "texto" ---> false
   // Tu código:
+  if (valor == null) {
+    return true;
+  } else return false;
+  if (valor == undefined) {
+    return true;
+  } else return false;
 }
 
 module.exports = esNuloOIndefinido;
