9a6a2a504c33d05083b9cbca23ded3d3b98006dd
JLopezTellez <jl764464@gmail.com>
2025-05-31 13:30:54 -0300

    Ejercicios C6 resueltos

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,16 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let min = Math.min(a, b); // Establece cual es el minimo numero entre a y b.
+  let max = Math.max(a, b); // Establece cual es el maximo numero entre a y b, para hacer el bucle de menor a mayor.
+  let producto = 1; // Empieza el producto de los números en 1. Si fuera 0 daría 0.
+
+  for (let i = min; i <= max; i++) { // i será el minimo entre a y b; mientras este sea menor  o igual al maximo; agregar uno a i.
+    producto *= i; // Es lo mismo que producto = producto * i
+  }
+
+  // Corregir -0 a 0
+  return producto === 0 ? 0 : producto;
 }
 
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -4,6 +4,14 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  if (numero < 1) return false; // Las potencias de 2 son positivas y mayores a 0
+
+  while (numero > 1) {
+    if (numero % 2 !== 0) return false; // Si no es divisible entre 2, no es potencia
+    numero = numero / 2;
+  }
+
+  return true; // Si llegamos a 1, era una potencia de 2
 }
 
 module.exports = esPotenciaDeDos;
